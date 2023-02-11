d7112d936b8969ac551b97432ddacdedaec8aa9a
Alexandr Litvinov <70410264+SNK-LT@users.noreply.github.com>
2023-02-11 23:59:10 +0300

    Nothing Important

--- src/main/java/com/example/pettimer/HelloController.java
+++ src/main/java/com/example/pettimer/HelloController.java
@@ -1,14 +1,35 @@
 package com.example.pettimer;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button changeUserNameButton;
+
+    @FXML
+    private Text userNameText;
+
+    @FXML
+    private TextField userNameTextField;
+
+    @FXML
+    void changeUserNameButtonClicked(MouseEvent event) {
+        if(userNameTextField.isVisible())
+        {
+            userNameText.setText(userNameTextField.getText());
+            userNameTextField.setVisible(false);
+            changeUserNameButton.setText("Change User Name");
+        }
+        else
+        {
+            userNameTextField.setVisible(true);
+            changeUserNameButton.setText("Accept");
+        }
     }
+
 }
