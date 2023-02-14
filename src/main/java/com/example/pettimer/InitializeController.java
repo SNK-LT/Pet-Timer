038a6d15144a175a6bf5b69430d404e6d13295be
Alexandr Litvinov <70410264+SNK-LT@users.noreply.github.com>
2023-02-15 01:14:33 +0300

    Add new Init animations

--- src/main/java/com/example/pettimer/InitializeController.java
+++ src/main/java/com/example/pettimer/InitializeController.java
@@ -1,8 +1,16 @@
 package com.example.pettimer;
 
 import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
+import javafx.beans.property.DoubleProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
@@ -21,10 +29,43 @@ public class InitializeController implements Initializable {
     private Circle circle2;
 
     @FXML
+    private Button loginButton;
+
+    @FXML
+    private AnchorPane slidingField;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
     private AnchorPane sideField;
 
     private double xOffset = 0;
     private double yOffset = 0;
+
+    @FXML
+    void loginButtonClicked(ActionEvent event) {
+
+        ScaleTransition st = new ScaleTransition(Duration.millis(300), slidingField);
+        st.setToX(1);
+        st.play();
+
+        TranslateTransition tt = new TranslateTransition(Duration.millis(300), slidingField);
+        tt.setToX(0);
+        tt.play();
+
+        /*slidingField.setMinWidth(400);
+        slidingField.setLayoutX(180);
+        slidingField.setPrefWidth(400);
+
+        Insets loginInsets = new Insets(35,86,140,165);
+        Insets passwordInsets = new Insets(75,86,100,165);
+
+        loginField.setPadding(loginInsets);*/
+    }
 
     private void setRotate(Circle c, boolean reverse, int angle, int duration)
     {
@@ -35,7 +76,7 @@ public class InitializeController implements Initializable {
         rt.setByAngle(angle);
         rt.setDelay(Duration.seconds(0));
         rt.setRate(3);
-        rt.setCycleCount(18);
+        rt.setCycleCount(-1);
         rt.play();
     }
 
@@ -58,5 +99,14 @@ public class InitializeController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         setRotate(circle1, true, 360, 18);
         setRotate(circle2, true, 360, 10);
+
+        ScaleTransition stFirst = new ScaleTransition(Duration.millis(300), slidingField);
+        stFirst.setToX(0.3);
+        stFirst.play();
+
+        TranslateTransition ttFirst = new TranslateTransition(Duration.millis(300), slidingField);
+        ttFirst.setToX(-200);
+        ttFirst.play();
+
     }
 }
