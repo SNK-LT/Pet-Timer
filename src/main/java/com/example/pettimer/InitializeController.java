f2c03e6da8cee78a5117d8205c79ac53b80833fb
Alexandr Litvinov <70410264+SNK-LT@users.noreply.github.com>
2023-02-15 06:07:33 +0300

    New Color Theme Hiding Password Animated elements README link

--- src/main/java/com/example/pettimer/InitializeController.java
+++ src/main/java/com/example/pettimer/InitializeController.java
@@ -10,13 +10,20 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import java.awt.Desktop;
+import java.net.URI;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -36,12 +43,19 @@ public class InitializeController implements Initializable {
 
     @FXML
     private TextField loginField;
-
     @FXML
-    private TextField passwordField;
+    private PasswordField passField;
+    @FXML
+    private TextField pass2Field;
 
     @FXML
     private AnchorPane sideField;
+
+    @FXML
+    private ImageView hidePassImage;
+    @FXML
+    private ImageView showPassImage;
+
 
     private double xOffset = 0;
     private double yOffset = 0;
@@ -49,22 +63,13 @@ public class InitializeController implements Initializable {
     @FXML
     void loginButtonClicked(ActionEvent event) {
 
-        ScaleTransition st = new ScaleTransition(Duration.millis(300), slidingField);
+        ScaleTransition st = new ScaleTransition(Duration.millis(200), slidingField);
         st.setToX(1);
         st.play();
 
-        TranslateTransition tt = new TranslateTransition(Duration.millis(300), slidingField);
+        TranslateTransition tt = new TranslateTransition(Duration.millis(200), slidingField);
         tt.setToX(0);
         tt.play();
-
-        /*slidingField.setMinWidth(400);
-        slidingField.setLayoutX(180);
-        slidingField.setPrefWidth(400);
-
-        Insets loginInsets = new Insets(35,86,140,165);
-        Insets passwordInsets = new Insets(75,86,100,165);
-
-        loginField.setPadding(loginInsets);*/
     }
 
     private void setRotate(Circle c, boolean reverse, int angle, int duration)
@@ -80,6 +85,25 @@ public class InitializeController implements Initializable {
         rt.play();
     }
 
+    @FXML
+    void hidePassword(MouseEvent event) {
+        hidePassImage.setVisible(true);
+        showPassImage.setVisible(false);
+
+        passField.setText(pass2Field.getText());
+        passField.setVisible(true);
+        pass2Field.setVisible(false);
+    }
+
+    @FXML
+    void showPassword(MouseEvent event) {
+        hidePassImage.setVisible(false);
+        showPassImage.setVisible(true);
+
+        pass2Field.setText(passField.getText());
+        pass2Field.setVisible(true);
+        passField.setVisible(false);
+    }
 
     @FXML
     void sideFieldDragged(MouseEvent event) {
@@ -95,18 +119,25 @@ public class InitializeController implements Initializable {
         yOffset = stage.getY() - event.getScreenY();
     }
 
+    @FXML
+    void helpClicked(ActionEvent event) throws URISyntaxException, IOException {
+        Desktop.getDesktop().browse(new URI("https://github.com/SNK-LT/PetTimer/blob/master/README.md"));
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         setRotate(circle1, true, 360, 18);
         setRotate(circle2, true, 360, 10);
 
-        ScaleTransition stFirst = new ScaleTransition(Duration.millis(300), slidingField);
+        ScaleTransition stFirst = new ScaleTransition(Duration.millis(1), slidingField);
         stFirst.setToX(0.3);
         stFirst.play();
 
-        TranslateTransition ttFirst = new TranslateTransition(Duration.millis(300), slidingField);
+        TranslateTransition ttFirst = new TranslateTransition(Duration.millis(1), slidingField);
         ttFirst.setToX(-200);
         ttFirst.play();
+
+
 
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.pettimer {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.desktop;
 
 
     opens com.example.pettimer to javafx.fxml;
